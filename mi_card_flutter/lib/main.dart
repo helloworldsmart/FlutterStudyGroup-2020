b5f16fa5a2eb2b12b92402b3b12bc40c9eace161
MichaelChen <helloworldsmart3@gmail.com>
2020-12-05 14:31:29 +0800

    Challenge: ListTile

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -39,51 +39,38 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.phone,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text('+44 123 456 789',
+                    style: TextStyle(
                       color: Colors.teal.shade900,
-                      // size: 100.0,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
                     ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text('+44 123 456 789',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.email,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade900,
+                  ),
+                  title: Text('helloworldsmart3@gmail.com',
+                    style: TextStyle(
                       color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
                     ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text('helloworldsmart3@gmail.com',
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ],
@@ -95,3 +82,13 @@ class MyApp extends StatelessWidget {
 }
 
 
+// padding: EdgeInsets.all(25.0),
+// child: Row(
+// children: [
+//
+// SizedBox(
+// width: 10.0,
+// ),
+//
+// ],
+// ),
