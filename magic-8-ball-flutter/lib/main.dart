331a936af05c0f0130fee7e1d7e654d4ad7be2f8
MichaelChen <helloworldsmart3@gmail.com>
2020-12-06 16:20:06 +0800

    magic-8-ball-flutter : 8-74 Done

---
+++ magic-8-ball-flutter/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: BallPage(),
+        ),
+      ),
+    );
+
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FlatButton(
+                child: Image(
+                  image: AssetImage('images/ball1.png'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
